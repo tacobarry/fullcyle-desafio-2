87671d8efd47f7f2a30821477ed03ebb091030db
Guilherme Michelan (Tacobarry) <guilherme.rodella@sensedia.com>
2021-10-27 10:59:12 -0300

    criando a tabela na aplicação

--- node/index.js
+++ node/index.js
@@ -16,8 +16,12 @@ const connection = mysql.createConnection(config)
 
 connection.connect();
 
-// const sql = `INSERT INTO people(name) values ('Tacobarry');`
-// connection.query(sql)
+let sql = `CREATE TABLE people(id int NOT NULL auto_increment prymary_key, name varchar(255) NOT NULL);`
+connection.query(sql)
+sql = `INSERT INTO people(name) values ('Tacobarry');`
+connection.query(sql)
+sql = `insert into people(name) values('Ensina a gente a fazer dump e rodar pra não fazer essa gamb aqui!');`
+connection.query(sql)
 
 app.get('/', (req, res) => {
   connection.query(`SELECT * from people`, function(err, results) {
